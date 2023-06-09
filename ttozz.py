fafbede71d1e03a820e31d6fd114c5d04f9556da
tdewin <tdewin@users.noreply.github.com>
2023-06-09 12:43:16 +0200

    Update ttozz.py
    more reporting like savings estimation

--- ttozz.py
+++ ttozz.py
@@ -4,12 +4,14 @@ import argparse
 import glob
 import subprocess
 import re
+import math
 
-def analyzeDir(jobdir,segmenting):
+def analyzeDir(jobdir,segmenting,kilo,exponent):
     print("Dir: %s Segment Size: %d" % (jobdir,segmenting))
     files = glob.glob("%s/*.v[bi][kb]" % jobdir)
 
     segments = set()
+    allsegcount = 0
 
     for f in files:
         bmap = subprocess.Popen(["xfs_bmap",f], stdout=subprocess.PIPE, universal_newlines=True)
@@ -22,19 +24,28 @@ def analyzeDir(jobdir,segmenting):
                 stop = int(m.group(2))
                 while start < stop:
                     segments.add(start)
+                    allsegcount += 1
                     start += segmenting 
             #else:
             #    print("ignoring:",i.strip())
 
     #All the file offsets and disk blocks are in units of 512-byte blocks or half a kb (/2)
-    overlaps = len(segments)*segmenting/2/1024/1024
-    print(overlaps)
+    segcount = len(segments)
+    humanize = math.pow(kilo,exponent)
+    suffix = ["K","M","G","T","P"]
+    overlaps = segcount*segmenting/2/humanize
+    fsreportusages = allsegcount*segmenting/2/humanize
+    savings = allsegcount/segcount
+
+    print("%s %sB (%d*%d*512); fs reported %s %sB; Savings %s:1" % (overlaps,suffix[exponent],segcount,segmenting,fsreportusages,suffix[exponent],savings))
 
 parser = argparse.ArgumentParser(
                     prog='XFS TTOZZ',
                     description='Analyses block cloning',
                     epilog='Pass a directory')
 parser.add_argument('-j','--jobdir', required=True)
-parser.add_argument('-s','--segmenting', default=256,type=int)
+parser.add_argument('-s','--segmenting', default=256,type=int,help="granularity of the calculation. Most accurate would be 1 but will take a long time. Use something above > 256 for scalability")
+parser.add_argument('-b','--bytedivider', default=(1024),type=int,help="Default 1024 for converting bytes to human readable.")
+parser.add_argument('-e','--expdivider',default=(2),type=int,help="Exponent to convert value to human readable (bytdivider)^expdivider. k=0,m=1,g=2,t=3,p=4")
 args = parser.parse_args()
-analyzeDir(args.jobdir,args.segmenting)
+analyzeDir(args.jobdir,args.segmenting,args.bytedivider,args.expdivider)
