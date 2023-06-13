9f8c0801adeabb230df3a5bb1f2d86718910bc1a
tdewin <tdewin@users.noreply.github.com>
2023-06-13 14:47:57 +0200

    Update sf-powershell.ps1

--- sortedfragments/sf-powershell.ps1
+++ sortedfragments/sf-powershell.ps1
@@ -1,9 +1,13 @@
-﻿$filefrag = "R:\r1\w32filefrag.exe"
+﻿$filefrag = "R:\w32filefrag.exe"
 $python = "C:\Users\Administrator\AppData\Local\Programs\Python\Python311\python.exe"
+$sfanalyze = "R:\sortedfrag-analyze.py"
+$cachefolder = "R:\sfcache"
 
 #block size, if you have been a good admin, you formated refs with 64 blocksize
 $b = 65536
 
+
+New-Item -ItemType Directory $cachefolder -ErrorAction SilentlyContinue | Out-Null
 
 [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
@@ -14,11 +18,14 @@ $foldername.ShowDialog()
 $folder = $foldername.SelectedPath
 $files = Get-ChildItem $folder | ? { $_.name -match ".(vbk|vib)$" }
 
-
+$cpath = Join-Path $cachefolder (Get-Item -Path $folder).BaseName
+New-Item -ItemType Directory $cpath -ErrorAction SilentlyContinue  | Out-Null
 
 foreach($f in $files) {
- . $filefrag $f.fullname | Set-Content -Encoding UTF8 ("{0}.frag" -f $f.BaseName)
+ . $filefrag $f.fullname | Set-Content -Encoding UTF8 (Join-Path $cpath ("{0}.frag" -f $f.BaseName))
 }
 
 
- sortedfrag-analyze.py -d "Backup Job 4" -b 65536
+. $python $sfanalyze  -d $cpath -b 65536
+
+read-host
