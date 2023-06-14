ea09cc8c27100f69c2575d37e3ad23b1d6737dbd
tdewin <tdewin@users.noreply.github.com>
2023-06-14 17:45:42 +0200

    Update sortedfrag-analyze.py
    allow filtering

--- sortedfragments/sortedfrag-analyze.py
+++ sortedfragments/sortedfrag-analyze.py
@@ -14,98 +14,101 @@ def parseLine(ln):
     return res
 
 
-def analyzeDir(idir,blksize,hints):
-    gpattern = idir+"/*.frag"
+def analyzeDir(idir,ffilter,blksize,hints):
+    gpattern = idir+"/"+ffilter
     files = glob.glob(gpattern)   
     print("---")
     print("basedir:",idir)
     print("frag_files_count: {}".format(len(files)))
-    print("frag_files_processed:")
-    stacks = {}
-    for f in files:
-        reader = open(f, 'r')
-        while reader.readline().strip() != "extents:":
-            #skip
-            pass
-        m = parseLine(reader.readline())
-        if m != None:
-            stacks[f] = {"reader":reader,"head":m}
-        else:
-            print("#error: Trouble seeking and finding first extend for",f)
-
-    real = 0
-    total = 0
-    working = [-1,-2]
-    accounted = True
-
-    while len(stacks) > 0:
-        c = None
-        cname = None
-        i = math.inf
-
-        for s in stacks:
-            n = stacks[s]
-            t = n["head"][0]
-            if t < i:
-                i = t
-                c = n
-                cname = s
-        
-        current = c["head"]
-        
-        #reported by all files
-        #interval 0..0 is 1
-        total += (current[1]-current[0])+1
-
-        if current[0] <= working[1]:
-            if working[0] <= current[0]:
-                working[1] = max(current[1],working[1])
+    if len(files) > 0:
+        print("frag_files_processed:")
+        stacks = {}
+        for f in files:
+            reader = open(f, 'r')
+            while reader.readline().strip() != "extents:":
+                #skip
+                pass
+            m = parseLine(reader.readline())
+            if m != None:
+                stacks[f] = {"reader":reader,"head":m}
             else:
-                print("#error: not sorted list detected")
-        else:
+                print("#error: Trouble seeking and finding first extend for",f)
+
+        real = 0
+        total = 0
+        working = [-1,-2]
+        accounted = True
+
+        while len(stacks) > 0:
+            c = None
+            cname = None
+            i = math.inf
+
+            for s in stacks:
+                n = stacks[s]
+                t = n["head"][0]
+                if t < i:
+                    i = t
+                    c = n
+                    cname = s
+            
+            current = c["head"]
+            
+            #reported by all files
             #interval 0..0 is 1
-            real += (working[1]-working[0])+1
-            working = current
-        
+            total += (current[1]-current[0])+1
 
-        ln = c["reader"].readline()
-        if ln:
-            m = parseLine(ln)
-            if m:
-                c["head"] = m
+            if current[0] <= working[1]:
+                if working[0] <= current[0]:
+                    working[1] = max(current[1],working[1])
+                else:
+                    print("#error: not sorted list detected")
             else:
-                print("#error: Read a line for",c,"but it didnt confirm.",ln)
-        else:
-            c["reader"].close()
-            print(" -",os.path.basename(cname))
-            stacks.pop(cname)
+                #interval 0..0 is 1
+                real += (working[1]-working[0])+1
+                working = current
+            
 
-    #add last interval
-    #interval 0..0 is 1
-    real += (working[1]-working[0])+1
-   
-    savings_rat = total/real
+            ln = c["reader"].readline()
+            if ln:
+                m = parseLine(ln)
+                if m:
+                    c["head"] = m
+                else:
+                    print("#error: Read a line for",c,"but it didnt confirm.",ln)
+            else:
+                c["reader"].close()
+                print(" -",os.path.basename(cname))
+                stacks.pop(cname)
 
-    blkkb = blksize/1024
-    #blksize is in kb
-    rkb = int(real*(blkkb))
-    tkb = int(total*(blkkb))
+        #add last interval
+        #interval 0..0 is 1
+        real += (working[1]-working[0])+1
+       
+        savings_rat = total/real
 
-    #(>>17)/8 is like dividing by 2^20 or kb to gb
-    # (int(math.pow(2,20)) >> 17)/8 = 1
-    print("real_clusters:",real)
-    if hints > 0:
-        print("# compare with used df \"/origdir\"")
-        print("# only works with single chain on fs or you need to sum_all_dirs(real_kb)")
-    print("real_kb:",rkb)
-    print("real_gb:",(rkb>>16)/16)
-    print("total_clusters:",total)
-    if hints > 0:
-        print("# compare with du \"/origdir\"")
-    print("total_kb:",tkb)
-    print("total_gb:",(tkb>>16)/16)
-    print("savings_rat:",savings_rat)
-    print("savings_gb:",(((tkb-rkb)>>16)/16))
+        blkkb = blksize/1024
+        #blksize is in kb
+        rkb = int(real*(blkkb))
+        tkb = int(total*(blkkb))
+
+        #(>>17)/8 is like dividing by 2^20 or kb to gb
+        # (int(math.pow(2,20)) >> 17)/8 = 1
+        print("real_clusters:",real)
+        if hints > 0:
+            print("# compare with used df \"/origdir\"")
+            print("# only works with single chain on fs or you need to sum_all_dirs(real_kb)")
+        print("real_kb:",rkb)
+        print("real_gb:",(rkb>>16)/16)
+        print("total_clusters:",total)
+        if hints > 0:
+            print("# compare with du \"/origdir\"")
+        print("total_kb:",tkb)
+        print("total_gb:",(tkb>>16)/16)
+        print("savings_rat:",savings_rat)
+        print("savings_gb:",(((tkb-rkb)>>16)/16))
+    else:
+        print("#error: couldn't find files")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -113,8 +116,9 @@ if __name__ == "__main__":
                     description='Sorted frag',
                     epilog='Pass a the directory with frag files')
     parser.add_argument('-d','--dir', required=True)
+    parser.add_argument('-f','--filter', default="*.frag")
     parser.add_argument('-b','--blksize', default=4096,type=int)
     parser.add_argument('--hints', default=0, type=int)
     args = parser.parse_args()
-    analyzeDir(args.dir,args.blksize,args.hints)
+    analyzeDir(args.dir,args.filter,args.blksize,args.hints)
 
